a64076e6ff27963c8d8267aa51a86f68cfa3f836
Dmitriy Kachko <dmkachko@gmail.com>
2021-12-10 18:57:27 -0500

    Re-create the project with the latest angular CLI (13.1) Upgrade to the latest Redux Add compatibility for the braking changes in the Redux (Symbol.observable property) Bump up Angular version

---
+++ projects/angular-redux-store/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of angular-redux-store
+ */
+
+export * from './lib/angular-redux-store.service';
+export * from './lib/angular-redux-store.component';
+export * from './lib/angular-redux-store.module';
